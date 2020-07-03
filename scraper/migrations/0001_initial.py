d1ab91a9ce9629eaa76eca8f9debcfde89afddfe
nwuensche <Wuensche.Niklas@gmail.com>
2020-07-03 20:00:08 +0200

    Add Migration Stuff DB

---
+++ scraper/migrations/0001_initial.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Mytest',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('myfield', models.DateTimeField(auto_now_add=True, verbose_name=b'my field')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Number',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('thenumber', models.IntegerField(verbose_name=b'the number')),
+            ],
+        ),
+    ]
