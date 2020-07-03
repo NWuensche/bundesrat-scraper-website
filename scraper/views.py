49203d05d4fc3dafaa10445d857089f59632c916
nwuensche <Wuensche.Niklas@gmail.com>
2020-07-03 20:05:15 +0200

    Add Index/Counter/Number Views

---
+++ scraper/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Mytest, Number
+from django.views.decorators.csrf import csrf_exempt
+
+def index(request):
+    return render(request, "index.html")
+
+@csrf_exempt  #TODO Remove annotation
+def update_counter(request):
+    cou = Mytest()
+    cou.save()
+
+    cou.save()
+    cous = Mytest.objects.all()
+    message = 'update successful {}x'.format("test")
+    return render(request, "mycounter.html", {"counters": cous})
+
+def sendMethod(request):
+    numbers = Number.objects.all()
+    if len(numbers) == 0:
+        number = Number()
+        number.save()
+    number = Number.objects.first() #get (maybe new) number, only one number in DB
+    oldNumber = number.number
+
+    number.number = request.POST["textfield"]
+    number.save()
+    return render(request, "number.html", {"oldNumber": oldNumber, "newNumber": number.number, "IO": request.POST["textfield"] })
+
+def tef(request=None):
+    pass
