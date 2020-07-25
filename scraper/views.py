7d720b8789f9c267809b7362f1ae825cb1c75174
nwuensche <Wuensche.Niklas@gmail.com>
2020-07-25 17:12:04 +0200

    Add View Method for AJAX get TOPs for given session Number

--- scraper/views.py
+++ scraper/views.py
@@ -36,6 +36,20 @@ def tef(request=None):
 def showDiagram(request):
     return render(request, "diagram.html", {"yes": 10, "no": 5, "out": 3})
 
+def getTopsAJAX(request):
+    jsons = Json.objects.all() #TODO Check if any, don't load all (Multiple methods here use this)
+    if len(jsons) == 0: #Load 
+        loadJSONsInDB()
+    sessionNumber = int(request.GET['sNumber'])
+    brRow = Json.objects.get(county="bundesrat")
+    brJSON = json.loads(brRow.json)
+    for session in brJSON:
+        if int(session['number']) == sessionNumber:
+            allTOPs = list(map(lambda top: {'name': top["number"]}, session["tops"]))
+            allTOPs.reverse() #TOP 1 at the start afterwards
+            break
+    return HttpResponse(json.dumps(allTOPs), content_type='application/json') #Doesn't recognize without content_type
+
 def loadJSONsInDB():
     counties = [
             "baden_wuerttemberg",
